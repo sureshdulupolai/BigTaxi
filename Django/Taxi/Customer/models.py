a281a325fa56125c0b65054395db5a70d8968eb9
Suresh Polai <sureshdulupolai@gmail.com>
2025-05-05 12:13:49 +0530

    creating a model for customer

--- Django/Taxi/Customer/models.py
+++ Django/Taxi/Customer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from Users.models import usersDataModel
 
 # Create your models here.
+class cutomerDataModel(models.Model):
+    UDM = models.ForeignKey(usersDataModel, on_delete=models.CASCADE)
+    totalTripCount = models.IntegerField(default=0)
+    userName = models.CharField(max_length=100, default='Big Taxi User')
+    userImage = models.ImageField(default='customer/default/logo.png', upload_to='customer/exist/', max_length=1000)
+    description = models.CharField(max_length=2000, default='No Description!')
+    
+    def __str__(self):
+        return str((self.UDM, self.totalTripCount))
