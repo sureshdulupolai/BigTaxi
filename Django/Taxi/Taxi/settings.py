0e62f3ed2b4e8bef4c59ff2b66261eb50ac699d9
Suresh Polai <sureshdulupolai@gmail.com>
2025-05-19 19:11:21 +0530

    static folder in django project

--- Django/Taxi/Taxi/settings.py
+++ Django/Taxi/Taxi/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Static files directories (required for project-level folder)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
